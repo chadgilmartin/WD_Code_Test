bbb6e1be5582490456a693c127610e0598f5dcd9
chadgilmartin <gilettawedding2014@gmail.com>
2016-03-16 14:45:50 -0400

    added bower and jQuery library

---
+++ FrontEndDeveloper_Test/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#shipping-company").hide();
+    $("#commercial").click(function(){
+        $("#shipping-company").show();
+    });
+    $("#residential").click(function(){
+        $("#shipping-company").hide();
+    });
+});
