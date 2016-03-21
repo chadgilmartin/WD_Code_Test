94afb707793952e899da54c3b16cd53494827262
chadgilmartin <gilettawedding2014@gmail.com>
2016-03-21 13:42:04 -0400

    added placeholders

--- FrontEndDeveloper_Test/js/scripts.js
+++ FrontEndDeveloper_Test/js/scripts.js
@@ -79,5 +79,27 @@ $(document).ready(function(){
 		});
 	});												// end of Submit button validations
 
-	
+	if(!Modernizr.input.placeholder) {				// if no HTML5 placeholder support, add them here
+		$('[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+				input.removeClass('placeholder');
+			}
+		}).blur(function() {
+			var input = $(this);
+			if (input.val() == '' || input.val() == input.attr('placeholder')) {
+				input.addClass('placeholder');
+				input.val(input.attr('placeholder'));
+			}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
+			$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() == input.attr('placeholder')) {
+					input.val('');
+				}
+			})
+		});
+	}
 });
