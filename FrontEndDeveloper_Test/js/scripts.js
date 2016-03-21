4e382c4088dfba236fff713154c8a64d4071c61e
chadgilmartin <gilettawedding2014@gmail.com>
2016-03-21 17:26:01 -0400

    add validation

--- FrontEndDeveloper_Test/js/scripts.js
+++ FrontEndDeveloper_Test/js/scripts.js
@@ -8,16 +8,38 @@ $(document).ready(function(){
 	});
 	$("#phone-error").hide();
 	$("#zip-error").hide();
+	$("#required-fields").hide();
+	
 	function isEmail(email) {						// function to verify proper email addresses
 		var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 		if(!regex.test(email)) {
-			return false;
 			console.log(email);
+			return false;
 		}
 		else {
 			return true;
 		}
-	}												// end of email verification
+	};												// end of email verification
+	function isPhone (phone) {
+		var numbers = /[0-9 -()+]+$/;
+		if (!numbers.test(phone) || phone.length != 10) {
+			console.log("phone: " + phone);
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+	function isZip (zip) {
+		var numbers = /[0-9 -()+]+$/;
+		if (!numbers.test(zip) || zip.length != 5) {
+			console.log("zip: " + zip);
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 
 	$("#state-select").blur(function () {			// test state selection for shipping
 		var state = $("#state-select").val();
@@ -28,15 +50,26 @@ $(document).ready(function(){
 		}
 		else {
 				$("#submit").show();
-		}
-	});												// end state selection test
+		}											// end state selection test
+	});
 
-	$("#submit").click(function (event) {			// validations for Submit button
+
+
+	function isValid() {							// validate form for submitting
+
 		var emailAddress = $("#email-address").val();
 		var emailConfirm = $("#email-address-confirm").val();
+		var phoneNumber = $("#phone").val();
+		var zipCode = $("#zip").val();
+		var emailCheck = false;
+		var phoneCheck = false;
+		var zipCheck = false;
+
+
 		if( isEmail(emailAddress) && emailAddress === emailConfirm ) {		// email validation
-			console.log(emailAddress);
-			console.log(emailConfirm);
+			console.log(emailAddress, emailConfirm);
+			emailCheck = true;
+			console.log("email is valid");
 		}
 		else if (!isEmail(emailAddress)) {
 			alert("Please enter a valid email address.");
@@ -44,42 +77,60 @@ $(document).ready(function(){
 		}
 		else if (emailAddress !== emailConfirm) {
 			alert("email addresses must match");
+		}
+		else {
+			console.log("email field failed")
 			return false;
 		}
-		$(".phone-test").keyup(function () {		// phone number validation
-			if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
-				$("#phone-error").show();
-				return false;
-				console.log(this.value);
-			}
-			else if (this.value.length != 10.1) {
-				$("#phone-error").show();
-				return false;
-				console.log(this.value);
-			}
-			else {
-				$("#phone-error").hide();
-			}
-		});
-		$(".zip-test").keyup(function () {			// zip code validation
-			if (this.value != this.value.replace(/[^0-9\.]/g, '')){
-				$("#zip-error").show();
-				return false;
-				console.log(this.value);
-			}
-			else if (this.value.length !=5) {
-				$("#zip-error").show();
-				return false;
-				console.log(this.value);
-			}
-			else {
-				$("#zip-error").hide();
+		if (isZip(zipCode)) {									// zip code validation
+			zipCheck = true;
+			$("#zip-error").hide();
+			console.log("zip code is valid");
+		}
+		else {
+			$("#zip-error").show();
+			console.log("zip: " + zipCode);
+			return false;
+		}
+		if (isPhone(phoneNumber)) {								// phone number validation
+			phoneCheck = true;
+			$("#phone-error").hide();
+			console.log("phone number is valid");
+		}
+		else {
+			$("#phone-error").show();
+			console.log("phone: " + phoneNumber);
+			return false;
+		}
+		if (emailCheck  && phoneCheck  && zipCheck) {
+			console.log("Submission is valid");
+			return true;
+		}
+		else {
+			console.log("submission error");
+			return false;
+		}
+	}											
 
-			}
-		});
-	});												// end of Submit button validations
+	 $("#submit").click( function() {						// validations for Submit button			
+		if(!Modernizr.input.required) {						// Add required fields support for clients without HTML5
+		console.log("required fields are filled out")
+		} 
+		else {
+			$('form').find('input[required]').each(function () {
+				$(this).attr('class', 'required ' + this.getAttribute('type'));
+				$("#required-fields").show();
+			});
+		}
+		if (isValid()) {
+			return true;
+		}
+		else return false;
 
-	if(!Modernizr.input.placeholder) {				// if no HTML5 placeholder support, add them here
+	});
+	
+
+	if(!Modernizr.input.placeholder) {							// Add placeholders for clients without HTML5 support
 		$('[placeholder]').focus(function() {
 			var input = $(this);
 			if (input.val() == input.attr('placeholder')) {
@@ -101,5 +152,5 @@ $(document).ready(function(){
 				}
 			})
 		});
-	}
+	}												// end of placeholders for clients without HTML5 
 });
